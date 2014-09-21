4d66021f04a0382f22cf2c2acc81c4d7f89c9e92
fumito_ito <weathercook@gmail.com>
2014-09-22 02:06:19 +0900

    setup access trace

--- EventHubSample/Controllers/HomeController.cs
+++ EventHubSample/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,11 +11,13 @@ namespace EventHubSample.Controllers
     {
         public ActionResult Index()
         {
+            Trace.WriteLine(String.Format("Request on /Home/Index by {0}", Request.UserAgent));
             return View();
         }
 
         public ActionResult About()
         {
+            Trace.WriteLine(String.Format("Request on /Home/About by {0}", Request.UserAgent));
             ViewBag.Message = "Your application description page.";
 
             return View();
@@ -22,6 +25,7 @@ namespace EventHubSample.Controllers
 
         public ActionResult Contact()
         {
+            Trace.WriteLine(String.Format("Request on /Home/Contact by {0}", Request.UserAgent));
             ViewBag.Message = "Your contact page.";
 
             return View();
