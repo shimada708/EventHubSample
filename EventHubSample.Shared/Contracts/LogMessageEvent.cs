bc89c9f632b092d38fd8aefd7398dacf79cfca68
fumito_ito <weathercook@gmail.com>
2014-09-21 23:36:07 +0900

    add classes to send message to Hub Service

---
+++ EventHubSample.Shared/Contracts/LogMessageEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHubSample.Shared.Contracts
+{
+    public class LogMessageEvent
+    {
+        public string MachineName { get; set; }
+        public string SiteName { get; set; }
+        public string InstanceId { get; set; }
+        public string Value { get; set; }
+    }
+}
