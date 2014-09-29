2b0753c46a591a6582e05be8790fdec33b7ce6ec
fumito_ito <weathercook@gmail.com>
2014-09-30 00:51:10 +0900

    fix using wrong token provider.

--- EventHubSample.Console/Program.cs
+++ EventHubSample.Console/Program.cs
@@ -24,32 +24,38 @@ namespace EventHubSample.Console
                 {
                     System.Console.WriteLine("Starting worker to process partition: {0}", state);
 
-                    var factory = MessagingFactory.Create(ServiceBusEnvironment.CreateServiceUri("sb", ConfigurationManager.AppSettings["ServiceBus.Namespace"], ""), new MessagingFactorySettings(){
-                        TokenProvider = TokenProvider.CreateSharedSecretTokenProvider(ConfigurationManager.AppSettings["ServiceBus.KeyName"], ConfigurationManager.AppSettings["ServiceBus.Key"]),
+                    var factory = MessagingFactory.Create(ServiceBusEnvironment.CreateServiceUri("sb", ConfigurationManager.AppSettings["ServiceBus.Namespace"], ""), new MessagingFactorySettings()
+                    {
+                        TokenProvider = TokenProvider.CreateSharedAccessSignatureTokenProvider(ConfigurationManager.AppSettings["ServiceBus.KeyName"], ConfigurationManager.AppSettings["ServiceBus.Key"]),
                         TransportType = TransportType.Amqp
                     });
 
                     var client = factory.CreateEventHubClient("logs");
-                    var grp = client.GetDefaultConsumerGroup();
+                    var group = client.GetDefaultConsumerGroup();
 
-                    System.Console.WriteLine("Group: {0}", grp.GroupName);
+                    System.Console.WriteLine("Group: {0}", group.GroupName);
 
-                    var receiver = grp.CreateReceiver(state.ToString(), DateTime.UtcNow);
+                    var receiver = group.CreateReceiver(state.ToString(), DateTime.UtcNow);
 
-                    while (true) {
+
+                    while (true)
+                    {
                         if (cts.IsCancellationRequested)
                         {
                             receiver.Close();
                             break;
                         }
 
+                        // Receive could fail, I would need a retry policy etc...
                         var messages = receiver.Receive(10);
                         foreach (var message in messages)
                         {
-                            var logMessage = JsonConvert.DeserializeObject<LogMessageEvent>(Encoding.Default.GetString(message.GetBytes()));
+                            var logMessage = Newtonsoft.Json.JsonConvert.DeserializeObject<LogMessageEvent>(Encoding.Default.GetString(message.GetBytes()));
 
                             System.Console.WriteLine("{0} [{6}] {2}/{3}: {5}", DateTime.Now, message.PartitionKey, logMessage.MachineName, logMessage.SiteName, logMessage.InstanceId, logMessage.Value, state);
                             System.Console.WriteLine(" > Instance/PartitionKey: {0}", message.PartitionKey);
+
+
                         }
                     }
                 }, i);
